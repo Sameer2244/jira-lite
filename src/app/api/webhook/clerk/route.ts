500fb1d6db92df04a87253ace558564e824a307b
sameersk <sk5276873@gmail.com>
2025-07-06 19:00:20 +0530

    database name fixed

--- src/app/api/webhook/clerk/route.ts
+++ src/app/api/webhook/clerk/route.ts
@@ -6,25 +6,25 @@ import { NextRequest, NextResponse } from "next/server";
 export async function POST(req: NextRequest) {
   try {
     const body = await req.json();
-    console.log(body.data.email_addresses[0].email_address);
-    console.log(body.data.id);
-    console.log(body.data.first_name);
-    console.log(body.data.last_name);
-    console.log(body.data.profile_image_url);
     // saving it in a mongodb if user is created
     if (body.type === "user.created") {
       const client = await clientPromise;
       const db = client.db("jira-users");
-      const collection = db.collection("users-collection");
+      const collection = db.collection("user-collection");
       const userToadded = {
         email: body.data.email_addresses[0].email_address,
-        id: body.data.id,
+        _id: body.data.id,
         first_name: body.data.first_name,
         last_name: body.data.last_name,
         profile_image_url: body.data.profile_image_url,
       };
-      console.log(userToadded);
       await collection.insertOne(userToadded);
+    }
+    if (body.type === "user.deleted") {
+      const client = await clientPromise;
+      const db = client.db("jira-users");
+      const collection = db.collection("user-collection");
+      await collection.deleteOne({ _id: body.data.id });
     }
 
     // Do something with the webhook data
